f87066963a928c3efacba5d8c34bfcd612454ea1
Hazey8709 <jfortin802@gmail.com>
2023-04-08 15:30:10 -0400

    first com

---
+++ services/todoService.test.js
@@ -0,0 +1,22 @@
+const { todoService, todoServiceId } = require("./todoService");
+jest.mock("../__mocks__/todoService");
+
+describe("Todo Service Test", () => {
+    test("User return 10 Todos", async () => {
+        const result = await todoService();
+
+        expect(result.data).toHaveLength(200);
+        expect(result.data[8].userId).toEqual(1);
+        expect(result.data[8].id).toEqual(9);
+        expect(result.data[8].title).toEqual("molestiae perspiciatis ipsa");
+        expect(result.data[8].completed).toEqual(false);
+    });
+
+    test("Return By ID", async () => {
+        const result = await todoServiceId(3);
+
+        expect(result.data.userId).toEqual(1);
+        expect(result.data.title).toEqual("fugiat veniam minus");
+        expect(result.data.completed).toEqual(false);
+    });
+});
